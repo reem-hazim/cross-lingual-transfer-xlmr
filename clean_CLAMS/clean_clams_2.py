4bcfa4eeabbc558165fd2af2bf416519c8057107
reem-hazim <reem.hazim02@gmail.com>
2021-05-16 00:16:08 +0400

    Fixed error

--- clean_CLAMS/clean_clams_2.py
+++ clean_CLAMS/clean_clams_2.py
@@ -23,8 +23,8 @@ test_df, val_df = train_test_split(test_and_val_df, test_size = 0.5)
 
 test_df["label"] = [int(label == True) for label in test_df["label"]]
 val_df["label"] = [int(label == True) for label in val_df["label"]]
-train_df["label"] = [int(label == "True") for label in train_df["label"]]
+train_df["label"] = [int(label == True) for label in train_df["label"]]
 
-train_df.to_csv("eng_train_2.csv", index=False)
-val_df.to_csv("eng_val_2.csv", index=False)
-test_df.to_csv("eng_test_.csv", index=False)
+train_df.to_csv("clean_CLAMS/eng_train_2.csv", index=False)
+val_df.to_csv("clean_CLAMS/eng_val_2.csv", index=False)
+test_df.to_csv("clean_CLAMS/eng_test_2.csv", index=False)
